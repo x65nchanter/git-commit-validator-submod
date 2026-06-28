3ad3bd64c5b53c5cdbdf3c385553140124ef98c9
x65nchanter <x65nchanter@gmail.com>
2026-06-28 20:58:10 +0300

    test(powershell): verify windows setup execution
    - Add install.Tests.ps1 specification via Pester

---
+++ install.Tests.ps1
@@ -0,0 +1,58 @@
+Describe "Git Commit Validator Windows Installer Specification" {
+    BeforeAll {
+        $Script:SandboxPath = Join-Path $env:TEMP "Git_Installer_Sandbox"
+        if (Test-Path $Script:SandboxPath) { Remove-Item $Script:SandboxPath -Recurse -Force }
+        $null = New-Item -ItemType Directory -Path (Join-Path $Script:SandboxPath ".git") -Force
+
+        Copy-Item "$PSScriptRoot\install.ps1" -Destination $Script:SandboxPath
+
+        Mock git {
+            return 0
+        }
+    }
+
+    AfterAll {
+        # Return terminal context to avoid PowerShell holding a handle on the directory
+        if ($PSScriptRoot) { Set-Location $PSScriptRoot }
+
+        # Secure and clean removal of the temporary directory execution context
+        if (Test-Path $Script:SandboxPath) {
+            Remove-Item $Script:SandboxPath -Recurse -Force
+        }
+    }
+
+    It "Should invoke the installation process and deploy the hook into a repository without pre-existing hooks" {
+        Push-Location $Script:SandboxPath
+
+        { .\install.ps1 -ProfileName "conventional-commits" } | Should -Not -Throw
+
+        Should -Invoke -CommandName 'git' -Exactly -Times 1 -ParameterFilter { "$args" -match 'config local.core.hooksPath .githooks' }
+        Should -Invoke -CommandName 'git' -Exactly -Times 1 -ParameterFilter { "$args" -match 'config local.commitValidator.format conventional-commits' }
+
+        Pop-Location
+    }
+
+    It "Should invoke the installation process and maintain stability when executed on a repository with a pre-installed module hook" {
+        Push-Location $Script:SandboxPath
+        Throw NotImplemented
+        Pop-Location
+    }
+
+    It "Should invoke the installation process and link the validation rules via the MANUAL hook integration strategy" {
+        Push-Location $Script:SandboxPath
+        Throw NotImplemented
+        Pop-Location
+    }
+
+    It "Should invoke the installation process and wrap the pre-existing hook via the BEFORE integration strategy to execute validation upstream" {
+        Push-Location $Script:SandboxPath
+        Throw NotImplemented
+        Pop-Location
+    }
+
+    It "Should invoke the installation process and wrap the pre-existing hook via the AFTER integration strategy to execute validation downstream" {
+        Push-Location $Script:SandboxPath
+        Throw NotImplemented
+        Pop-Location
+    }
+}
